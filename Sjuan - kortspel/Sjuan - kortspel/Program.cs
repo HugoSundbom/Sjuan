e84dd7075e51e12d9ac785cd3f04371404b8e8fc
Maxpajnert <91895122+Maxpajnert@users.noreply.github.com>
2022-09-14 08:46:45 +0200

    Add files via upload

---
+++ Sjuan - kortspel/Sjuan - kortspel/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sjuan___kortspel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Tuple<int, string> > Kortlek = new List<Tuple<int, string> >();
+
+            string colour = "hjärter";
+            int colourcount = 1;
+
+            for (int i = 2; i < 15; i++)
+            {
+                Kortlek.Add(new Tuple<int, string>(i, colour));
+                if (i == 14 && Kortlek.Count < 14)
+                {
+                    i = 1;
+                }
+            }
+
+            Kortlek.Sort();
+            
+            for(int i = 0; i < Kortlek.Count; i++)
+            {
+                Console.WriteLine(Kortlek[i]);
+            }
+
+            Console.ReadKey();
+           }
+    }
+}
