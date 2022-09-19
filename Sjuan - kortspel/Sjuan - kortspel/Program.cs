38e7a93327c77ca28778b466253d01990dad5c77
Hugo Sundbom <hugo.sundbom@elev.ga.ntig.se>
2022-09-19 10:50:14 +0200

    Commit

--- Sjuan - kortspel/Sjuan - kortspel/Program.cs
+++ Sjuan - kortspel/Sjuan - kortspel/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Sjuan___kortspel
 {
@@ -7,6 +8,7 @@ namespace Sjuan___kortspel
     {
         static void Main(string[] args)
         {
+            //Skapar Kortlek
             List<Tuple<int, string> > Kortlek = new List<Tuple<int, string> >();
 
             string colour = "hjärter";
@@ -33,12 +35,87 @@ namespace Sjuan___kortspel
 
             Kortlek.Sort();
             
-            for(int i = 0; i < Kortlek.Count; i++)
+            /*for(int i = 0; i < Kortlek.Count; i++)
             {
                 Console.WriteLine(Kortlek[i]);
-            }
+            }*/
 
             Console.ReadKey();
-           }
+
+            List < Tuple<int, string> > player1 = Sorter(Kortlek);
+            List < Tuple<int, string> > player2 = Sorter(Kortlek);
+            List < Tuple<int, string> > player3 = Sorter(Kortlek);
+            List < Tuple<int, string> > player4 = Sorter(Kortlek);
+            for(int i = 0; i < player1.Count; i++)
+            {
+                Console.WriteLine(player1[i]);
+                Console.WriteLine(player2[i]);
+            }
+            Kortcheck(player1, player2);
+            Kortcheck(player1, player3);
+            Kortcheck(player1, player4);
+            Kortcheck(player2, player3);
+            Kortcheck(player2, player4);
+            Kortcheck(player3, player4);
+
+            string startingPlayer = "hej";
+            if (whoStart(player1) == true) { startingPlayer = "player1"; }
+            else if (whoStart(player2) == true) { startingPlayer = "player2"; }
+            else if (whoStart(player2) == true) { startingPlayer = "player3"; }
+            else if (whoStart(player2) == true) { startingPlayer = "player4"; }
+            Console.WriteLine(startingPlayer);
+            //spel
+
+        }
+        static List<Tuple<int, string>> Sorter(List<Tuple<int, string>> Kortlek)
+        {
+            List<Tuple<int, string>> Hand = new List<Tuple<int, string>>();
+
+            Random rnd = new Random();
+            for (int i = 0; i < 13; i++){
+                int rand = rnd.Next(0, Kortlek.Count);
+                Hand.Add(Kortlek[rand]);
+                Kortlek.Remove(Kortlek[rand]);
+            }
+            return Hand;
+        }
+
+        static bool whoStart(List<Tuple<int, string>>player)
+        {
+            bool playerstart = false;
+            for (int i = 0; i < player.Count; i++)
+            {
+                Tuple<int, string> hjärter7 = new Tuple<int, string>(7,"hjärter");
+                if (player[i] == hjärter7)
+                {
+                     playerstart = true;
+                }
+            }
+            return playerstart;
+
+        }
+        static void Kortcheck(List<Tuple<int, string>>p1, List<Tuple<int, string>>p2)
+        {
+            int error = 0;
+            for(int i = 0; i < p1.Count; i++)
+            {
+                for(int j = 0; i < p1.Count; i++)
+                {
+                    if (p1[i] == p2[j])
+                    {
+                        error++;
+                    }
+                }
+            }
+            if(error > 0)
+            {
+                Console.WriteLine("ERROR COPIES " + error);
+            }
+            else
+            {
+                Console.WriteLine("NO COPIES");
+            }
+        }
+            
     }
 }
